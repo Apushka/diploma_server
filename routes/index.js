66d31a0f344840e643fc45b8a0e02d299c67d35d
Александр Пушкин <aleksandrpushkin@MacBook-Air-Aleksandr.local>
2022-11-30 13:44:54 +0300

    auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+router.use("/auth", require("./auth.routes"));
 router.use("/category", require("./category.router"));
 router.use("/tag", require("./tag.routes"));
 router.use("/product", require("./product.routes"));
